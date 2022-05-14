18b48ee7dad7305ca53871758c939237035509ae
T ontheTop <tuanak691@gmail.com>
2022-05-14 11:15:50 +0700

    ss

--- routes/upload.js
+++ routes/upload.js
@@ -12,49 +12,50 @@ const storage = multer.diskStorage({
     },
     filename: function(req, file, cb) {
         console.log(file.originalname);
-        if (!file.originalname.startsWith("NAPA")) throw new Error("name invalid");
-        const fileType = file.originalname.split(".")[1];
-        cb(null, `${file.fieldname}-${Date.now()}.${fileType}`);
+        let error = null;
+        if (!file.originalname.startsWith("NAPA")) error = "Name invalid";
+        const fileType = file.originalname.split(".").pop();
+        cb(
+            error ? new Error(error) : null,
+            `${file.fieldname}-${Date.now()}.${fileType}`
+        );
     },
 });
 const maxSize = 1 * 1024 * 1024;
 
 var upload = multer({
     storage: storage,
-    limits: { fileSize: maxSize }
+    limits: { fileSize: maxSize },
 });
 
 const uploadMiddlewareMutiple = (req, res, next) => {
     upload.array("multi-files")(req, res, function(err) {
         if (err) {
-            res.json({ error: err.message });
+            return res.json({ error: err.message });
         }
-        next()
+        next();
     });
-}
+};
 const uploadMiddlewareSingle = (req, res, next) => {
     upload.single("single-file")(req, res, function(err) {
         if (err) {
-            res.json({ error: err.message });
+            return res.json({ error: err.message });
         }
-        next()
+        next();
     });
-}
+};
 
-uploadRouter.post(
-    "/multiple", uploadMiddlewareMutiple,
-    async(req, res) => {
-        try {
-            console.log(req.files)
-            let result = await (await getDbInstance())
-                .collection("uploads")
-                .insertMany(req.files);
-            res.send({ fileId: result.insertedIds });
-        } catch (error) {
-            res.json({ error: error.message });
-        }
+uploadRouter.post("/multiple", uploadMiddlewareMutiple, async(req, res) => {
+    try {
+        console.log(req.files);
+        let result = await (await getDbInstance())
+            .collection("uploads")
+            .insertMany(req.files);
+        res.send({ fileId: result.insertedIds });
+    } catch (error) {
+        res.json({ error: error.message });
     }
-);
+});
 
 uploadRouter.post("/", uploadMiddlewareSingle, async(req, res) => {
     try {
